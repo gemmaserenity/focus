11bca79119381252c4cd904241a42172678e7222
Sascha Gorokhoff <gorokhoff.gemma@gmail.com>
2026-06-21 13:36:32 -0700

    briefing: midday update 2026-06-21 13:36

--- sw.js
+++ sw.js
@@ -1,30 +1,20 @@
-// Focus PWA service worker
 const CACHE = 'focus-v1';
-
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(['/', '/manifest.json']))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/manifest.json'])));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request)
-      .then(r => {
-        const clone = r.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return r;
-      })
-      .catch(() => caches.match(e.request))
+    fetch(e.request).then(r => {
+      const clone = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return r;
+    }).catch(() => caches.match(e.request))
   );
 });
